eb154bc45a37d014c78d58d48df826de763f8a4d
blackfirefly000 <50463948+blackfirefly000@users.noreply.github.com>
2024-08-28 00:33:40 -0700

    Update fxmanifest.lua
    Update version

--- boatHUD/fxmanifest.lua
+++ boatHUD/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'blackfirefly000'
 description 'Boat HUD and Anchor script with speed, heading, coordinates, depth, and more'
-version '1.0.0'
+version '1.0.1'
 
 server_script 'server.lua'
 client_script 'client.lua'
